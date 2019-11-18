d7d4d6c022e7131cedbb0a71ea0d5a935410bb0b
Damon Shaw <admins@vt.edu>
2019-11-17 21:39:42 -0500

    added new explosion animation frames

--- sprites/explosionAnimation.js
+++ sprites/explosionAnimation.js
@@ -1,39 +1,35 @@
 function ExplosionAnimation(xPos, yPos) {
     this.position = createVector(xPos, yPos);
 
-    this.frame0 = ExplosionFrame0;
+    this.frames = [
+        ExplosionFrame0, ExplosionFrame1, ExplosionFrame2,
+        ExplosionFrame3, ExplosionFrame4, ExplosionFrame5,
+        ExplosionFrame6, ExplosionFrame7
+    ];
+
 
     this.frame = 0;
 
     this.draw = function() {
-        switch(this.frame) {
-            case 0:
-                image(this.frame0, this.position.x, this.position.y);
-            break;
-            case 1:
-                // Do nothing.
-            break;
-            case 2:
-                image(this.frame0, this.position.x, this.position.y);
-            break;
-            case 3:
-                // Do nothing.
-            break;
-            case 4:
-                image(this.frame0, this.position.x, this.position.y);
-            break;
-            case 5:
-                // Do nothing.
-            break;
-            default:
-                // Draw nothing.
-            break;
-        }
+        if(this.isDone()) return;
 
-        this.frame++;
+        imageMode(CENTER);
+
+        let frame = this.frames[this.frame];
+        image(
+            frame,
+            this.position.x,
+            this.position.y
+        );
+
+        imageMode(CORNER);
+
+        if(frameCount % 5 === 0)
+            this.frame++;
+
     }
 
     this.isDone = function() {
-        return this.frame >= 6;
+        return this.frame >= this.frames.length;
     }
 }
